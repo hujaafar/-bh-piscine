527fe1a4df95ebc47c0280e0c83f4e06d0cd9e52
hujaafar <hjawady703@gmail.com>
2024-02-19 16:19:34 +0300

    q2

--- isnegative.go
+++ isnegative.go
@@ -3,10 +3,10 @@ package piscine
 import "github.com/01-edu/z01"
 
 func IsNegative(nb int) {
-	if nb > 0 {
-		z01.PrintRune(70)
+	if nb < 0 {
+		z01.PrintRune('T')
 	} else {
-		z01.PrintRune(84)
+		z01.PrintRune('F')
 	}
 	z01.PrintRune('\n')
 }
