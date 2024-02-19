bc50da0a9e7892f976094723c655c71b5023f4a1
hujaafar <hjawady703@gmail.com>
2024-02-19 15:57:22 +0300

    q2c4

--- isnegative.go
+++ isnegative.go
@@ -3,10 +3,10 @@ package main
 import "github.com/01-edu/z01"
 
 func IsNegative(nb int) {
-if nb > 0 {
-	z01.PrintRune(84)
-} else {
-z01.PrintRune(70)
-}
-z01.PrintRune('\n')
+	if nb > 0 {
+		z01.PrintRune(84)
+	} else {
+		z01.PrintRune(70)
+	}
+	z01.PrintRune('\n')
 }
