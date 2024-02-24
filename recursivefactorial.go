45606ef5070f4bebb1d0aa544141b51f08efb00b
hujaafar <hjawady703@gmail.com>
2024-02-24 15:41:05 +0300

    q4q2

---
+++ recursivefactorial.go
@@ -0,0 +1,15 @@
+package piscine
+
+func RecursiveFactorial(nb int) int {
+	if nb == 0 {
+		return 1
+	}
+	if nb == 1 {
+		return 1
+	}
+	if nb <= 20 && nb >= 0 {
+		return nb * IterativeFactorial(nb-1)
+	} else {
+		return 0
+	}
+}
