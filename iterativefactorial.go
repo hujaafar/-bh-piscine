1780ea2af17948fba4429bead972f53104da21ec
hujaafar <hjawady703@gmail.com>
2024-02-24 15:32:40 +0300

    q4

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,22 +1,14 @@
 package piscine
 
 func IterativeFactorial(nb int) int {
-	if nb < 0 { // Negative values are not allowed
+	if nb <= 0 {
 		return 0
 	}
-
-	if nb == 0 {
+	if nb > 20 {
+		return 0
+	}
+	if nb == 1 {
 		return 1
 	}
-
-	num := 1
-	for i := 1; i <= nb; i++ {
-		// Check for potential overflow
-		if num > (1<<31-1)/i {
-			return 0
-		}
-		num = num * i
-	}
-
-	return num
+	return nb * IterativeFactorial(nb-1)
 }
