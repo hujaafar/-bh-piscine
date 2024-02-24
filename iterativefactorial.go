7004b4ea83ca4dce6af2f5de69089448b6927ac3
hujaafar <hjawady703@gmail.com>
2024-02-24 15:22:33 +0300

    q4

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -2,11 +2,10 @@ package piscine
 
 func IterativeFactorial(nb int) int {
 	num := 1
-
+	if nb == 0 {
+		return 0
+	}
 	for i := 1; i <= nb; i++ {
-		if nb == 0 {
-			return 0
-		}
 		num = num * i
 	}
 	return num
