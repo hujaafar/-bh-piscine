58d5ef66de3968312768572b41aa839a8ee57e8f
hujaafar <hjawady703@gmail.com>
2024-02-24 16:14:18 +0300

    rec

--- recursivepower.go
+++ recursivepower.go
@@ -7,9 +7,6 @@ func RecursivePower(nb int, power int) int {
 	if power < 0 {
 		return 0
 	}
-	if nb <= 20 && nb >= 0 {
-		return nb * RecursivePower(nb, power-1)
-	} else {
-		return 0
-	}
+
+	return nb * RecursivePower(nb, power-1)
 }
