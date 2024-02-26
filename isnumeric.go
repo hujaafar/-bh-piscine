5c413fadb9a8b60d206c877d4d7fab5e26141ed7
hujaafar <hjawady703@gmail.com>
2024-02-26 16:28:33 +0300

    q5a12

--- isnumeric.go
+++ isnumeric.go
@@ -3,7 +3,7 @@ package piscine
 func IsNumeric(s string) bool {
 	s1 := []rune(s)
 	for i := 0; i < len(s1); i++ {
-		if s1[i] < 48 && s1[i] > 57 {
+		if s1[i] < 48 || s1[i] > 57 {
 			return false
 		}
 	}
