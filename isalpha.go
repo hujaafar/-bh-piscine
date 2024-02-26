e859b5c89448b797ade3b73b665cd46da0907b20
hujaafar <hjawady703@gmail.com>
2024-02-26 16:16:44 +0300

    q5a11

--- isalpha.go
+++ isalpha.go
@@ -3,8 +3,8 @@ package piscine
 func IsAlpha(s string) bool {
 	s1 := []rune(s)
 
-	for index := range s1 {
-		if index >= 20 && index <= 47 {
+	for i := 0; i < len(s1); i++ {
+		if s1[i] >= 20 && s1[i] <= 47 {
 			return false
 		}
 	}
