97d5928fe68b731e77db3233d040e0edd1988e54
hujaafar <hjawady703@gmail.com>
2024-02-26 15:18:42 +0300

    q5a7

--- index.go
+++ index.go
@@ -1,7 +1,12 @@
 package piscine
 
-import "strings"
-
 func Index(s string, toFind string) int {
-	return strings.Index(s, toFind)
+	for i := 0; i < len(s); i++ {
+		if i+len(toFind) <= len(s) {
+			if s[i:(i+len(toFind))] == toFind {
+				return i
+			}
+		}
+	}
+	return -1
 }
