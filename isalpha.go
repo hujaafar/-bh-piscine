52d861a944d074ceaa62f42c174b01cf0cdbbd0b
hujaafar <hjawady703@gmail.com>
2024-02-26 16:03:44 +0300

    q5a11

---
+++ isalpha.go
@@ -0,0 +1,12 @@
+package piscine
+
+func IsAlpha(s string) bool {
+	s1 := []rune(s)
+
+	for _, ch := range s1 {
+		if ch >= 'a' && ch > 'z' || ch >= 'A' && ch > 'Z' {
+			return true
+		}
+	}
+	return false
+}
