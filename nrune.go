19a4ab37d816ff2907d818718da1c2000bf3ef89
hujaafar <hjawady703@gmail.com>
2024-02-26 13:58:11 +0300

    q5a5

--- nrune.go
+++ nrune.go
@@ -2,12 +2,9 @@ package piscine
 
 func NRune(s string, n int) rune {
 	e := []rune(s)
-	if n > len(e) {
+	if n < 0 || n > len(e) {
 		return 0
-	} else if n < 0 {
-		n *= -n
-		return rune(s[len(s)-n])
 	} else {
-		return e[n]
+		return e[n-1]
 	}
 }
