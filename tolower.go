cc0f46c66b37fc0b0d6b74642411ce56c29ded6a
hujaafar <hjawady703@gmail.com>
2024-02-26 17:05:37 +0300

    q5a15

---
+++ tolower.go
@@ -0,0 +1,12 @@
+package piscine
+
+
+func ToLower(s string) string {
+	s1 := []rune(s)
+	for i := 0; i < len(s1); i++ {
+		if s1[i] >= 'A' && s1[i] <= 'Z' {
+			s1[i] += 32
+		}
+	}
+	return string(s1)
+}
