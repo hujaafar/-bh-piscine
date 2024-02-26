03390bdb8d7f94363b1138ae779eac8e98deba75
hujaafar <hjawady703@gmail.com>
2024-02-26 16:04:56 +0300

    q5a11

--- isalpha.go
+++ isalpha.go
@@ -4,9 +4,9 @@ func IsAlpha(s string) bool {
 	s1 := []rune(s)
 
 	for _, ch := range s1 {
-		if ch >= 'a' && ch > 'z' || ch >= 'A' && ch > 'Z' {
-			return true
+		if ch < 'a' && ch > 'z' || ch < 'A' && ch > 'Z' {
+			return false
 		}
 	}
-	return false
+	return true
 }
