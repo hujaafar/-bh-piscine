56ebc5e45b6cceee81cc06777bf4350ce51958e9
hujaafar <hjawady703@gmail.com>
2024-02-26 17:03:47 +0300

    q5a14

--- toupper.go
+++ toupper.go
@@ -3,8 +3,8 @@ package piscine
 func ToUpper(s string) string {
 	s1 := []rune(s)
 	for i := 0; i < len(s1); i++ {
-		if s1[i] >= 'a' || s1[i] <= 'z' {
-			return string(s1[i] - 32)
+		if s1[i] >= 'a' && s1[i] <= 'z' {
+			s1[i] -= 32
 		}
 	}
 	return string(s1)
