0551c8d51cd60bf52259c3643f7f7ab64a9ce620
hujaafar <hjawady703@gmail.com>
2024-03-06 16:10:57 +0300

    comp

--- descendcomb.go
+++ descendcomb.go
@@ -1,22 +1,30 @@
 package piscine
 
-import "github.com/01-edu/z01"
+import (
+	"github.com/01-edu/z01"
+)
 
 func DescendComb() {
-	for i := '9'; i >= '0'; i-- {
-		for b := '9'; b >= '0'; b-- {
-			for c := '9'; c >= '0'; c-- {
-				for d := '9'; d >= '0'; d-- {
-					z01.PrintRune(i)
-					z01.PrintRune(b)
-					z01.PrintRune(' ')
-					z01.PrintRune(c)
-					z01.PrintRune(d)
-					z01.PrintRune(',')
-					z01.PrintRune(' ')
+	for tens1 := '9'; tens1 >= '0'; tens1-- {
+		for ones1 := '9'; ones1 >= '0'; ones1-- {
+			for tens2 := tens1; tens2 >= '0'; tens2-- {
+				for ones2 := '9'; ones2 >= '0'; ones2-- {
+					if tens1*10+ones1 > tens2*10+ones2 {
+						printTwoDigitNumber(tens1)
+						printTwoDigitNumber(ones1)
+						z01.PrintRune(' ')
+						printTwoDigitNumber(tens2)
+						printTwoDigitNumber(ones2)
+						z01.PrintRune(',')
+						z01.PrintRune(' ')
+					}
 				}
 			}
 		}
 	}
-	z01.PrintRune('\n')
+}
+
+func printTwoDigitNumber(num rune) {
+	z01.PrintRune((num / 10) + '0')
+	z01.PrintRune((num % 10) + '0')
 }
