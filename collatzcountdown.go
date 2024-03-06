df2df0f7e679252561ffd69973226eca59719529
hujaafar <hjawady703@gmail.com>
2024-03-06 20:20:46 +0300

    c

---
+++ collatzcountdown.go
@@ -0,0 +1,21 @@
+package piscine
+
+func CollatzCountdown(start int) int {
+	count := 0
+	if start < 1 {
+		return -1
+	}
+	if start >= 1 {
+
+		if start%2 == 0 {
+			start /= 2
+			count++
+			if start%2 == 1 {
+				start = (start * 3) + 1
+				count++
+			}
+		}
+	}
+
+	return count
+}
