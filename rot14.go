f487a4a7f17e042631e5eb5aca953c1f8a8f1da3
hujaafar <hjawady703@gmail.com>
2024-03-06 16:02:28 +0300

    rot14

--- rot14.go
+++ rot14.go
@@ -1,10 +1,14 @@
 package piscine
 
 func Rot14(s string) string {
-	s1:=[]rune(s)
-	for i, chr:=range s{
-		if chr >='a' && chr<='z' || chr >='A' && chr <='Z'{
-			s1[i]+=14
+	s1 := []rune(s)
+	for i, chr := range s1 {
+		if chr >= 'a' && chr <= 'z' {
+			// Rotate lowercase letters
+			s1[i] = ((chr - 'a' + 14) % 26) + 'a'
+		} else if chr >= 'A' && chr <= 'Z' {
+			// Rotate uppercase letters
+			s1[i] = ((chr - 'A' + 14) % 26) + 'A'
 		}
 	}
 	return string(s1)
