592a82021358ec7e377c9f18d6d0c34d329cd9e4
hujaafar <hjawady703@gmail.com>
2024-03-06 16:14:19 +0300

    comp

--- descendcomb.go
+++ descendcomb.go
@@ -7,21 +7,26 @@ import (
 func DescendComb() {
 	for tens1 := '9'; tens1 >= '0'; tens1-- {
 		for ones1 := '9'; ones1 >= '0'; ones1-- {
-			for tens2 := tens1; tens2 >= '0'; tens2-- {
+			for tens2 := '9'; tens2 >= '0'; tens2-- {
 				for ones2 := '9'; ones2 >= '0'; ones2-- {
-					if tens1*10+ones1 > tens2*10+ones2 {
-						printTwoDigitNumber(tens1)
-						printTwoDigitNumber(ones1)
-						z01.PrintRune(' ')
-						printTwoDigitNumber(tens2)
-						printTwoDigitNumber(ones2)
-						z01.PrintRune(',')
-						z01.PrintRune(' ')
+					if tens1 != tens2 || ones1 != ones2 {
+						if tens1*10+ones1 > tens2*10+ones2 {
+							printTwoDigitNumber(tens1)
+							printTwoDigitNumber(ones1)
+							z01.PrintRune(' ')
+							printTwoDigitNumber(tens2)
+							printTwoDigitNumber(ones2)
+							if tens1 != '0' || ones1 != '1' || tens2 != '0' || ones2 != '0' {
+								z01.PrintRune(',')
+								z01.PrintRune(' ')
+							}
+						}
 					}
 				}
 			}
 		}
 	}
+	z01.PrintRune('\n')
 }
 
 func printTwoDigitNumber(num rune) {
