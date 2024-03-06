07fa46d1e828088b1f815f76642fdf8599e38135
hujaafar <hjawady703@gmail.com>
2024-03-07 00:51:21 +0300

    loa

--- loafofbread.go
+++ loafofbread.go
@@ -12,21 +12,16 @@ func LoafOfBread(str string) string {
 		for j := 0; j < 5; j++ {
 			result = append(result, rune(str[i+j]))
 		}
-		if i < len(str)-5 && str[i+5] != ' ' {
-			result = append(result, ' ')
-			i++ // Skip the next character
+		if i < len(str)-5 {
+			result = append(result, '\n') // Add newline after each set of 5 characters
 		}
 		i += 6 // Move to the next set of 5 characters
 	}
 
-	// Trim leading and trailing spaces
-	start, end := 0, len(result)
-	for start < len(result) && result[start] == ' ' {
-		start++
-	}
-	for end > start && result[end-1] == ' ' {
-		end--
+	// Trim trailing newline if present
+	if len(result) > 0 && result[len(result)-1] == '\n' {
+		result = result[:len(result)-1]
 	}
 
-	return string(result[start:end])
+	return string(result)
 }
