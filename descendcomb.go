123abfa0e7013d45b6d5ccfcd6ee7ff75ca7e69d
hujaafar <hjawady703@gmail.com>
2024-03-07 00:43:39 +0300

    de

--- descendcomb.go
+++ descendcomb.go
@@ -5,31 +5,19 @@ import (
 )
 
 func DescendComb() {
-	for tens1 := '9'; tens1 >= '0'; tens1-- {
-		for ones1 := '9'; ones1 >= '0'; ones1-- {
-			for tens2 := '9'; tens2 >= '0'; tens2-- {
-				for ones2 := '9'; ones2 >= '0'; ones2-- {
-					if tens1 != tens2 || ones1 != ones2 {
-						if tens1*10+ones1 > tens2*10+ones2 {
-							printTwoDigitNumber(tens1)
-							printTwoDigitNumber(ones1)
-							z01.PrintRune(' ')
-							printTwoDigitNumber(tens2)
-							printTwoDigitNumber(ones2)
-							if tens1 != '0' || ones1 != '1' || tens2 != '0' || ones2 != '0' {
-								z01.PrintRune(',')
-								z01.PrintRune(' ')
-							}
-						}
-					}
-				}
+	for i := 99; i >= 1; i-- {
+		for j := i - 1; j >= 0; j-- {
+			z01.PrintRune('0' + rune(i/10))
+			z01.PrintRune('0' + rune(i%10))
+			z01.PrintRune(' ')
+
+			z01.PrintRune('0' + rune(j/10))
+			z01.PrintRune('0' + rune(j%10))
+
+			if !(i == 1 && j == 0) {
+				z01.PrintRune(',')
+				z01.PrintRune(' ')
 			}
 		}
 	}
-	z01.PrintRune('\n')
-}
-
-func printTwoDigitNumber(num rune) {
-	z01.PrintRune((num / 10) + '0')
-	z01.PrintRune((num % 10) + '0')
 }
