5b892f6ae074391e0cc9efe8d9e44ead2e9f2cf8
hujaafar <hjawady703@gmail.com>
2024-03-06 20:41:41 +0300

    c

--- collatzcountdown.go
+++ collatzcountdown.go
@@ -1,21 +1,19 @@
 package piscine
 
 func CollatzCountdown(start int) int {
-	count := 0
-	if start < 1 {
+	if start <= 0 {
 		return -1
 	}
 
-	if start >= 1 {
+	count := 1
 
+	for start != 1 {
 		if start%2 == 0 {
-			start /= 2
-			count++
-			if start%2 == 1 {
-				start = (start * 3) + 1
-				count++
-			}
+			start = start / 2
+		} else {
+			start = 3*start + 1
 		}
+		count++
 	}
 
 	return count
