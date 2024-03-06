38e3e9bca4e1ec2ea834f499c4e3a967d1ee915d
hujaafar <hjawady703@gmail.com>
2024-03-06 21:38:10 +0300

    abo

---
+++ abort.go
@@ -0,0 +1,13 @@
+package piscine
+
+func Abort(a, b, c, d, e int) int {
+	arr := []int{a, b, c, d, e}
+	for i := 0; i < len(arr); i++ {
+		for j := i + 1; i < len(arr); i++ {
+			if arr[j] > arr[i] {
+				arr[i], arr[j] = arr[j], arr[i]
+			}
+		}
+	}
+	return arr[2]
+}
