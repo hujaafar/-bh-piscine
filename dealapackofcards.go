d189ef14e0414e55b314c888c24ad79d07eb8b1c
hujaafar <hjawady703@gmail.com>
2024-03-06 23:06:02 +0300

    dea

--- dealapackofcards.go
+++ dealapackofcards.go
@@ -5,12 +5,12 @@ import "fmt"
 func DealAPackOfCards(deck []int) {
 	player := 1
 	for j := 0; j < 12; {
-		fmt.Printf("Player %d:", player)
-		for i := 0; i < 4; i++ {
+		fmt.Printf("Player %d: ", player)
+		for i := 1; i < 4; i++ {
 			fmt.Printf("%d", deck[j])
 			j++
 			if i != 3 {
-				fmt.Printf(",")
+				fmt.Printf(", ")
 			}
 		}
 		player++
