76ce63b6a37e9cd975667c7746f697a618edc47a
hujaafar <hjawady703@gmail.com>
2024-03-06 21:44:22 +0300

    abo

--- abort.go
+++ abort.go
@@ -2,12 +2,18 @@ package piscine
 
 func Abort(a, b, c, d, e int) int {
 	arr := []int{a, b, c, d, e}
-	for i := 0; i < len(arr)-1; i++ {
-		for j := i + 1; i < len(arr)-1; i++ {
-			if arr[j] > arr[i] {
+	for i := 0; i < len(arr); i++ {
+		for j := i + 1; j < len(arr); j++ {
+			if arr[j] < arr[i] {
 				arr[i], arr[j] = arr[j], arr[i]
 			}
 		}
 	}
-	return arr[2]
+
+	n := len(arr)
+	if n%2 == 1 {
+		return arr[n/2]
+	}
+
+	return (arr[n/2-1] + arr[n/2]) / 2
 }
