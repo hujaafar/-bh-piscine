4cf4593b943b81b781adc2b275cc52fecc89f9a4
hujaafar <hjawady703@gmail.com>
2024-03-06 23:00:24 +0300

    dea

--- dealapackofcards.go
+++ dealapackofcards.go
@@ -3,15 +3,17 @@ package piscine
 import "fmt"
 
 func DealAPackOfCards(deck []int) {
-	cardsPerPlayer := len(deck) / 4
-	for i := 0; i < 4; i++ {
-		fmt.Printf("Player %d: ", i+1)
-		for j := 0; j < cardsPerPlayer; j++ {
-			if j != 0 {
-				fmt.Print(", ")
+	player := 1
+	for j := 0; j < 12; {
+		fmt.Printf("Player %d:", player)
+		for i := 0; i < 4; i++ {
+			fmt.Printf("%d", deck[j])
+			j++
+			if i != 3 {
+				fmt.Printf(",")
 			}
-			fmt.Print(deck[i*cardsPerPlayer+j])
 		}
-		fmt.Print("\n")
+		player++
+		fmt.Printf("\n")
 	}
 }
