a9ae5c867708bdd1461ddd95bbfb2cc843faebd5
hujaafar <hjawady703@gmail.com>
2024-03-07 00:49:42 +0300

    loa

--- loafofbread.go
+++ loafofbread.go
@@ -1,27 +1,32 @@
 package piscine
 
-import "strings"
-
 func LoafOfBread(str string) string {
 	if len(str) < 5 {
 		return "Invalid Output"
 	}
 
-	result := ""
+	var result []rune
 	i := 0
 
 	for i < len(str)-4 {
-		result += str[i : i+5]
-		i += 5 // Move to the next set of 5 characters
-
-		if i < len(str) {
-			if str[i] != ' ' {
-				result += string(str[i])
-				i++ // Skip the next character
-			}
-			result += " "
+		for j := 0; j < 5; j++ {
+			result = append(result, rune(str[i+j]))
 		}
+		if i < len(str)-5 && str[i+5] != ' ' {
+			result = append(result, ' ')
+			i++ // Skip the next character
+		}
+		i += 6 // Move to the next set of 5 characters
 	}
 
-	return strings.TrimSpace(result)
+	// Trim leading and trailing spaces
+	start, end := 0, len(result)
+	for start < len(result) && result[start] == ' ' {
+		start++
+	}
+	for end > start && result[end-1] == ' ' {
+		end--
+	}
+
+	return string(result[start:end])
 }
