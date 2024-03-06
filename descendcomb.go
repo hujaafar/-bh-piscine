e62f4b937b8871e77ca8f01c086c469db6a41c58
hujaafar <hjawady703@gmail.com>
2024-03-06 16:08:09 +0300

    comp

--- descendcomb.go
+++ descendcomb.go
@@ -3,17 +3,20 @@ package piscine
 import "github.com/01-edu/z01"
 
 func DescendComb() {
-	for i := '9'; i <= '0'; i-- {
-		for b := '9'; i <= '0'; i-- {
-			for c := '9'; i <= '0'; i-- {
-				for d := '9'; i <= '0'; i-- {
-					z01.PrintRune(i*10 + b)
+	for i := '9'; i >= '0'; i-- {
+		for b := '9'; b >= '0'; b-- {
+			for c := '9'; c >= '0'; c-- {
+				for d := '9'; d >= '0'; d-- {
+					z01.PrintRune(i)
+					z01.PrintRune(b)
 					z01.PrintRune(' ')
-					z01.PrintRune(c*10 + d)
+					z01.PrintRune(c)
+					z01.PrintRune(d)
 					z01.PrintRune(',')
 					z01.PrintRune(' ')
 				}
 			}
 		}
 	}
+	z01.PrintRune('\n')
 }
